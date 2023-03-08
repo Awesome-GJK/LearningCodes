eb53a3e62e67a385a8c5ee1ee5b870e6e7a4fa56
Awesome-GJK <gjk375675802@gmail.com>
2023-03-08 16:39:42 +0800

    测试

--- demo_lean/src/main/java/Test.java
+++ demo_lean/src/main/java/Test.java
@@ -1,16 +1,5 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.alibaba.fastjson.JSON;
 
 
 /**
@@ -25,7 +14,7 @@ public class Test {
 
 
     public static void main(String args[]) {
-
+//test
         List<String> list = Collections.emptyList();
         System.out.println(false);
 
@@ -33,4 +22,6 @@ public class Test {
 
 
 }
+
+
 
